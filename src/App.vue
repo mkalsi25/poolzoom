c695c443c575c8960c8de26caa9a34b101ab66cc
Manmeet <manmeetkalsi17@gmail.com>
2021-01-21 22:37:03 +0530

    update

--- src/App.vue
+++ src/App.vue
@@ -22,38 +22,272 @@
           src="@/assets/sd.png"
           class="w-full h-64 object-contain bg-gray-100"
         />
-        <h3 class="text-xl font-semibold px-4">
-          WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
-        </h3>
+        <div class=" px-4">
+          <h3 class="text-lg font-semibold">
+            WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
+          </h3>
+          <span class="text-xl text-primary">$620.00</span
+          ><span class=" line-through text-black-80 px-2">$650.35</span>
+        </div>
       </div>
       <div>
         <img
           src="@/assets/sd.png"
-          class="w-full h-64 object-contain  bg-gray-100"
+          class="w-full h-64 object-contain bg-gray-100"
         />
-        <h3 class="text-xl font-semibold px-4">
-          WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
-        </h3>
+        <div class=" px-4">
+          <h3 class="text-lg font-semibold">
+            WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
+          </h3>
+          <span class="text-xl text-primary">$620.00</span
+          ><span class=" line-through text-black-80 px-2">$650.35</span>
+        </div>
       </div>
       <div>
         <img
           src="@/assets/sd.png"
-          class="w-full h-64 object-contain  bg-gray-100"
+          class="w-full h-64 object-contain bg-gray-100"
         />
-        <h3 class="text-xl font-semibold px-4">
-          WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
-        </h3>
+        <div class=" px-4">
+          <h3 class="text-lg font-semibold">
+            WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
+          </h3>
+          <span class="text-xl text-primary">$620.00</span
+          ><span class=" line-through text-black-80 px-2">$650.35</span>
+        </div>
       </div>
       <div>
         <img
           src="@/assets/sd.png"
-          class="w-full h-64 object-contain  bg-gray-100"
+          class="w-full h-64 object-contain bg-gray-100"
         />
-        <h3 class="text-xl font-semibold px-4">
-          WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
-        </h3>
+        <div class=" px-4">
+          <h3 class="text-lg font-semibold">
+            WF-6 WhisperFlo Pool Pump 1.5HP FR 115/230V
+          </h3>
+          <span class="text-xl text-primary">$620.00</span
+          ><span class=" line-through text-black-80 px-2">$650.35</span>
+        </div>
       </div>
     </div>
+    <div class="grid">
+      <div
+        class="col-start-1 row-start-1 h-full md:h-96 py-12 md:py-0 relative gap-10 z-10 bg-black bg-opacity-25 grid grid-cols-1 md:grid-cols-3 items-center px-12 lg:px-48 "
+      >
+        <div>
+          <img
+            src="@/assets/truck.png"
+            class="p-2 shadow rounded-full bg-blue-600 mx-auto"
+          />
+          <h3
+            class="text-center text-white font-semibold py-3 text-xl px-8 lg:px-24"
+          >
+            Fast & Free shipping for orders over $75.
+          </h3>
+        </div>
+        <div>
+          <img
+            src="@/assets/truck.png"
+            class="p-2 shadow rounded-full bg-blue-600 mx-auto"
+          />
+          <h3
+            class="text-center text-white font-semibold py-3 text-xl px-8 lg:px-24"
+          >
+            Fast & Free shipping for orders over $75.
+          </h3>
+        </div>
+        <div>
+          <img
+            src="@/assets/truck.png"
+            class="p-2 shadow rounded-full bg-blue-600 mx-auto"
+          />
+          <h3
+            class="text-center text-white font-semibold py-3 text-xl px-8 lg:px-24"
+          >
+            Fast & Free shipping for orders over $75.
+          </h3>
+        </div>
+      </div>
+      <div class="col-start-1 row-start-1 h-full md:h-96  relative z-0">
+        <img
+          src="@/assets/shiping.jpg"
+          alt=""
+          class="h-full w-full object-cover"
+        />
+      </div>
+    </div>
+    <div class="py-12">
+      <h3 class="text-4xl text-center py-6">Featured Brands</h3>
+      <div class="flex items-center justify-center overflow-x-auto">
+        <img src="@/assets/brand.png" />
+        <img src="@/assets/2.png" />
+        <img src="@/assets/brand.png" />
+        <img src="@/assets/2.png" />
+        <img src="@/assets/brand.png" />
+        <img src="@/assets/2.png" />
+      </div>
+    </div>
+    <div class="py-12 bg-gray-50">
+      <h3 class="text-4xl text-center py-6">
+        Shop by Department
+      </h3>
+      <div class="grid grid-cols-1 md:grid-cols-4 px-4 ">
+        <div>
+          <img src="@/assets/prod.png" class="mx-auto h-48 w-auto" />
+          <h4 class="text-xl text-center">Chemicals</h4>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/2723/8714/0665e03f7095f56bc78a595d25581a21?Expires=1612137600&Signature=B3FyRhr-Qxs16uBSC5eZ282VERPI~F8HFT9xq4OyCTxvdTWhP4UNvYYN4j7-cXxtainFXxC1mi3U2~eOyAbwqUm8YAStEKOx~INQEjFbB2fmsHK5sfjYonZIdsLn~XzYwpRqy2vw2p-dcAxlx1zfJ8cACHYEQhINwE1yjMW4UuIyKEePWPctgnGc8e68VVP6zIZL0Ca4gSR7YTwdxroEjt~kHUQ-ynp-7TUI27EAQjPKu5gXG2J-7gWJGmbTCPMkZD9MgqtJCm6kJmD7iy-EWaqpCwOzh2tvm4BWWtcyoE7AlunuTbGMwcIve6ExhNEdIY0ZLWTvJUKf57wY1kEUUA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="mx-auto h-48 w-auto"
+          />
+          <h4 class="text-xl text-center">Equipment</h4>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/3b88/77e6/caf4298043b61f40bd7412341889a998?Expires=1612137600&Signature=CJxrwD5KYGKc53NIy~LzONr1rF6CcU5OCbCa6r3JLyMlKPUDXeD7IjXqNA8GtEONtHVXf9TW41tRThj~omz8MwbKj8SlBNx4VpkVQau1cqdYGKw0C7TBZMv4GexH8kEf5JDG~jFh9QNsf9NWGkvfi0VupC4xVhuODjbBmBaRQ80V3IpTQRPp9GrOC0bp~G0reS6UGhPDXbOA6QLjmuMLNrr5ZtQNDyFRtZ5CBFKoVVqDBCCXXrJ0iHhuRqfNEeDjiUpk2UnDp7pYS6kkaQxsqW4YWsfDif6R13klIymuuMPzseKsVsC9jxcaLTuGujYTny~WTXleKHxr9Nt6cgskNA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="mx-auto h-48 w-auto"
+          />
+          <h4 class="text-xl text-center">Parts & Hardware</h4>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/e9a3/4275/ff1c4d859e03a9b5ec2358840f9c9d32?Expires=1612137600&Signature=HPmq1Id~l2SjJiWnw9oUd9soPwxxY~NRB59DS2MrUg3ejAw6z8h5dinnmUNUY9Pah0iSX71oyA1C~jhvjHKqLSP7Z8meLEBKOTB6YG3YBmFvcWoeYwJ~buyMLJ4Z7tzR5uLIBnD0W3jwLt~XP5U83DZxrCg1Mazkmw4Ul38wJVkPsWPN8CKaYoCYgfONhfKLUUSTKWpxYxTkd81iXy4zgUfzjRSpGwfiOOhdErrECS3MwSKPSBB-VVYhyIM6ldt0YxaOG~vIuGibu8vwyr7uURQEQia8qOR6LB2pCh6ZlENouD~JndIsdgVmc4W9hkripcKvRrQ0iP7S49eMUr63mA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="mx-auto h-48 w-auto"
+          />
+          <h4 class="text-xl text-center">Accessories</h4>
+        </div>
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-4 px-4 lg:px-16">
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/d15b/358a/d4923379c554fbf570bc82b2bca805f7?Expires=1612137600&Signature=LkpJvpwK2kHXIXgRet9oXsuI5aMaYJ-EFH7swXI8nJiaulxihI8o1cxfVUQCsO5JADYID~04QAhSPfbLjWZDbYcmfo-g~Wr1dppTQwLfxEoA7QO4h2hm2WFIJVxlrEkwi6W-j9o0wJ1ccwnTCu-Fhcf3QhsNWpdZbNGaXG65gAZnN2LQyE8JN824TGtuQRfvEs4qet7spfK10784MaI4HUjuENavU4-ZPsCth9UVUIPL-foPleONNTyxR0oRKl9KxI~Q4ivWKkzWfZ9EhiBUbEtgMyqPYIVRpmLgc3sbXQFYSWBLYjF7OFo1NMcVOFYTHKmg8DjkkJw-fblmY9YwkA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="mx-auto h-48 w-auto"
+          />
+          <h4 class="text-xl text-center">Above Ground Pools</h4>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/9db8/bb8e/8df7160d0a0795e8eaa47258300602c1?Expires=1612137600&Signature=c93sFRg8lpv7ImcZw6N7ujdyAg5OL65zNLjiKM9pMmyDqV6Ttm8IBk~CrS3Xo-Mcfrrgdo3nYLMCIvVrZeKuzMuZecuIFE5J3NCg5s0FRsFMChvLxiLDftK351WbkG1E5DJ7yPM-1n9qHtQqYe7UOMJbCvH9VfZ6PBtLnntKZo72B~oav7848pwcG-585DTkKxSL-o1zdiB7u5SWo5erqLvxjhkn2bGT8Hyj4kV9f9XBijd9OSlwvRjoOFJVLJnABJ~WmctbTLmksP34SyOmm77s2EIeUvVdJWRV54ovXcr~WXoZFAvdDTkrsY380kNHsFZ7G1OXri71FP1v1iLMSw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="mx-auto h-48 w-auto"
+          />
+          <h4 class="text-xl text-center">Pool Covers</h4>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/06d9/6e74/fe527730f549b523eb61bf05b668a173?Expires=1612137600&Signature=cesqTAEFsDWyTpFPL2RRwLBc9nWEBz-Il7SuMKNjFq5Spt-CfKbBecJAs2XCrhphM6w3AK0hdRmlLd-kUpbOyJWvbvY~RC~rZyczpEy5d1K0SU1ofSaOnu4Dd2BtsE~ae3YH~1rXBMhTI2QbqGF0hGPa8xdgNMWDfrRbtmYA6dqvZmgPmQj~BFZdpLzCjiLmmL2AZaCpdywms5~Jgmw1zgOm9kNp9YuSX-ubz~4Et8eUrjNrXf5KEsQqv5B8axqJ7Je1ENXOUKA5lhCKb6vOS2~y8CZi3B2CpRz-p7E6Ti12wGd4x3qvuTPCS7foDNg9TEQNllrd4UYFcQu5UMgR2w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="mx-auto h-48 w-auto"
+          />
+          <h4 class="text-xl text-center">Spa Supplies</h4>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/5690/87ce/301601a7d77d03e99d8d62afe260b21b?Expires=1612137600&Signature=dKWzhrezb5lHbtyYr-R-P8VRC7WcKO27E7KOaTANsHw5U9r9Emr0FSCzcZWh4QZ-j-LnYOChQBw2TK6URhqWvsjk-z6DtvlEOvyRnBaM6kTOwWscF9mEiTl1RtTBDAXNkWMbHhBsspNf3SAk21ndakWU-V8dxoj5q5RprsteJQ-vQpAYTPqgqFs01gOzt0aVaZiaC2HLkhczdKV2Mc8VtDQCF9dKOLUUrd5PT3L6jgLvPtpisdpedabm93~SD1xzW0jmgItvb9y3rP9im3K4oedhhyC4ecn~VFpyVLslXpJJQxxmB14nLRxxCIWVU8UwKHkfWbH6P5uf79vGF~Hm6A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="mx-auto h-48 w-auto"
+          />
+          <h4 class="text-xl text-center">Brands</h4>
+        </div>
+      </div>
+    </div>
+    <div class="py-12">
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 gap-10 px-0 md:px-12 lg:px-24"
+      >
+        <div class="col-start-1 row-start-1 relative z-0">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/d901/54d3/49a52745d9e4d04c7a458eb592077e15?Expires=1612137600&Signature=es9~xPraWOLg9du~-1wpnd0Cnytmwv1Ys6o8vcsNLMc8E6XcUn~OxGmzzSO6WOwsuggf7jDtDnDjnMNgTAjCQw3HQUXGd8v4CMfKSH9Gps37aBSWmn-5~cKx16dOOb4KJYApxNpzBfY~Fwg4SJkfsF35BuJvm3A9e5jQQNyGf20-IOJMQpLBBgNIWSkk9HUiRZ3RX9TDz~8-YLj2Wt~V1dHDejZoQxt5-BUYKgj9fRXZn~Wbkiz4~zfbloOcgJQjsDRkSFjohkHu8aJ6jvXWbPP8u7K~pkY4IccMlli58x04g-oiybzm5jd~ZY5GNPBSs~suCdxkLs-zIf5YB29MMw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="w-full h-96 object-cover"
+          />
+        </div>
+        <div class="col-start-2 row-start-1 relative z-0">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/d901/54d3/49a52745d9e4d04c7a458eb592077e15?Expires=1612137600&Signature=es9~xPraWOLg9du~-1wpnd0Cnytmwv1Ys6o8vcsNLMc8E6XcUn~OxGmzzSO6WOwsuggf7jDtDnDjnMNgTAjCQw3HQUXGd8v4CMfKSH9Gps37aBSWmn-5~cKx16dOOb4KJYApxNpzBfY~Fwg4SJkfsF35BuJvm3A9e5jQQNyGf20-IOJMQpLBBgNIWSkk9HUiRZ3RX9TDz~8-YLj2Wt~V1dHDejZoQxt5-BUYKgj9fRXZn~Wbkiz4~zfbloOcgJQjsDRkSFjohkHu8aJ6jvXWbPP8u7K~pkY4IccMlli58x04g-oiybzm5jd~ZY5GNPBSs~suCdxkLs-zIf5YB29MMw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="w-full h-96 object-cover"
+          />
+        </div>
+        <div
+          class="bg-black bg-opacity-50 h-96 relative z-10 col-start-1 row-start-1 grid justify-items-center items-center"
+        >
+          <div>
+            <h4 class="text-center text-white text-4xl px-12">
+              Have a question about a product or assembly?
+            </h4>
+            <p class="text-white text-center pt-12">
+              Our Customer Support team is here to help.
+            </p>
+          </div>
+          <button
+            class="px-6 py-3 bg-primary rounded-full outline-none focus:outline-none text-center text-white"
+          >
+            Call Customer Support
+          </button>
+        </div>
+        <div
+          class="bg-black bg-opacity-50 h-96 relative z-10 col-start-2 row-start-1 grid justify-items-center items-center"
+        >
+          <div>
+            <h4 class="text-center text-white text-4xl px-12">
+              Create an account to unlock special features
+            </h4>
+            <p class="text-white text-center pt-12">
+              Our Customer Support team is here to help.
+            </p>
+          </div>
+          <button
+            class="px-6 py-3 bg-primary rounded-full outline-none focus:outline-none text-center text-white"
+          >
+            Call Customer Support
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="py-12">
+      <h3 class="text-4xl text-center py-6">Latest from the PoolZoom Blog</h3>
+      <div
+        class="grid grid-cols-2 md:grid-cols-4 px-0 gap-8 md:px-12 lg:px-24 py-6"
+      >
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/93f2/70ed/6b369489aaa6b40bf3511231d5778337?Expires=1612137600&Signature=WVN5c-xHn2QZztM4Q-cyStSH4cstlcRBGR2DQ0J4AVqHQIMT09JynB6l4dCGAL0VqoFqI1vBb94fw79MZZofS0SOyi2Jik2bZc2bqmJpKyL9b~sVMykqVqwmunDkaPrNudfaH~jhOit6ZKiksYkYWu6yv7fSpJol2mA0nzWwJTbj9FPLoRA2LFSS9Sbbo7C0okdx6K50z3tVi-Yo-QaCA5gBHdwNHKgh1wnJiNgS7ZENmCX0PUwp63tyi-o-fiEiOh1tTLo4OcEbhthj1Dc0NbJCRoP4ZJ8df2-LqlL3XhdpxdxazeGv2jObY024aRUFIkRgiGHVzcOlFKuGNqUiJA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="w-full h-64 object-cover"
+          />
+          <h4 class="text-xl font-medium py-3">Keep It Friendly</h4>
+          <p>Pool Products for the Eco-Conscious Consumer.</p>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/b4b9/ab53/cbdb59144c3b9fac633a6e4ba16f912e?Expires=1612137600&Signature=dhsR8~t6YOmMWRXp8Tn2EgvktFw0ypNdylhPu8fl8aQ2Vw8ckCq~AqW2-hnxItudR-MYauE5fRB6auCgMyQiV1zlEn17itaxMkyiwglsX9pGpej0SGrxKgJ6fPtd3hU38gVDonvWbmxf3ijhKJ2N0xHRcxrWISSWdMIlPrcYAnXiQFq3HhfwtBNewdT~rbEspogGZeFDVHVHpQ7djUcBMBJdGU1eD6ebZCswMhsMf6MWL11dTu4BfH36xGPkNPoDH9J2nCihpSzlMFjx1XK78atJ6EvsULDY00GTd4N3yOPuzsLqM5RVkZrJrVgZ310LY1Ax~RMJScaheQyMb-SaiA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="w-full h-64 object-cover"
+          />
+          <h4 class="text-xl font-medium py-3">Keep It Friendly</h4>
+          <p>Pool Products for the Eco-Conscious Consumer.</p>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/68b9/bdef/0528b50651d2f9904b3dd8042eb67e59?Expires=1612137600&Signature=aoYWwP4rWVOtScTPDE~pmpE4sHiTaM6Z2I-f1j--8zHWlh4oqBgRs6wqXgbLQHbrOOyXmFFHABwVGZxtb3VIsCLfi3xafQR1DXnBIueL5hEMicgxmN0GcCfL6G~lCLTWr3X86lp1-SwCip3lp8dqq7PI~ATqQhbkoP1VOI1wq8SKMKNCs6R7Rfe1cCkG6gavsrvGGlto~tpkD6iBji3GcSbennxfeeKfKRElYAHFjfRuGERl4PhjHA0nEmtWHQx-~jRFc83xGJPmX5-5gM15U~k7fyLeSFEZvCL2kp~UU9bDHCbneJaJ6ZCoEznDLs8ffKEOco7bbe3FvfMIopu5WQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="w-full h-64 object-cover"
+          />
+          <h4 class="text-xl font-medium py-3">Keep It Friendly</h4>
+          <p>Pool Products for the Eco-Conscious Consumer.</p>
+        </div>
+        <div>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/c006/3742/3a46c4c8a2730abd7cfd576f81b01c6e?Expires=1612137600&Signature=doj~v1i-gUXTdRGMxu6vz~yQcVAnY7aT-cjwFJM56cSopp6N1RdPSddcXdkabKIDjVmUT5LtHEeicr7AX0uzk0i3uY6FpFHT5nBI6f3hZM-HH7VVI5nZWgKUFWvCGp0KEE4N6oQCSirBjE-ua3UdImEKftwB1eoCpLg~RL4-m3h4YRaSnJA3dR6vYzU9nypflOJZr8A4zIfI-QyUmph8eQprEHJpGYiW462987pn7kHnMQCHsj8jgOb03Nxh9fHJ-KnrTOmluZmYC3T0S2oJ2dRd6cN3oEHEFy7sANnRX2czPHkl85awAuUOHzAxE20XylGJZHkHzorxna6QCNq5FQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+            class="w-full h-64 object-cover"
+          />
+          <h4 class="text-xl font-medium py-3">Keep It Friendly</h4>
+          <p>Pool Products for the Eco-Conscious Consumer.</p>
+        </div>
+      </div>
+      <div class="grid justify-items-center py-6">
+        <button
+          class="px-6 py-3 bg-primary rounded-full outline-none focus:outline-none text-center text-white"
+        >
+          View more from our blog
+        </button>
+      </div>
+    </div>
+    <div class="py-12 bg-black-80"></div>
   </div>
 </template>
 
